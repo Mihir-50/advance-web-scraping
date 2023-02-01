6cb4e29216b8a510d7e5b2b5f7847b8c771d3221
mihir <mihir231216@gmail.com>
2023-02-01 19:17:08 +0530

    from this code I was able to freeze screen opening through selenium.

--- campusx.py
+++ campusx.py
@@ -13,18 +13,44 @@ from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 # Import 'Keys' method, so that we can press any Key of computer.
 from selenium.webdriver.common.keys import Keys
+#
+from selenium.webdriver.chrome.options import Options
+
+
+def browser_function(url="https://www.google.com"):
+    # define the driver path
+    driver_path = Service("C:/Users/mihir/Desktop/chromedriver.exe")
+    # set the different options for the browser
+    chrome_options = Options()
+    chrome_options.add_experimental_option("detach", True)
+    chrome_options.add_experimental_option('excludeSwitches', ['enable-logging'])
+    # ignore the certificate and SSL errors
+    chrome_options.add_argument('--ignore-certificate-errors')
+    chrome_options.add_argument('--ignore-ssl-errors')
+    # maximize the browser window
+    chrome_options.add_argument("start-maximized")
+    # define with the driver and open the browser
+    chrome_driver = webdriver.Chrome(service=driver_path, options=chrome_options)
+    # open the Google search page, for Google search engine
+    chrome_driver.get(url)
+    # then return the driver object
+    return chrome_driver
 
 # Create a Service which is using 'chromedriver'.
 # define an object of 'Service' class & pass path of 'chromedriver'.
 # convert every backward slash into forward slash.
-s = Service("C:/Users/mihir/Desktop/chromedriver.exe")
+# s = Service("C:/Users/mihir/Desktop/chromedriver.exe")
 
 # Passing Service to below code
 # to open new Chrome window automatically, pass 'chromedriver' as a service
-driver = webdriver.Chrome(service = s)
+# driver = webdriver.Chrome(service = s)
+
+# from below code we are doing same thing what we are doing with above two & one below commented code.
+driver = browser_function()
 
 # to open any URL
-driver.get('http://google.com')
+#driver.get('http://google.com')
+
 time.sleep(1.5)
 
 # fetch the 'search input box' of google using xpath which is found using inspect.
@@ -46,4 +72,7 @@ time.sleep(1)
 # code which automatically find 'login'.
 link2 = driver.find_element(by=By.XPATH,value='/html/body/div[1]/header/section[2]/a[5]')
 link2.click()
-time.sleep(200)
+
+
+
+
